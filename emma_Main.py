84b7e5b1f8a4996907f85a680f4b126057724ae9
nomfnomf <emma.j.martin@outlook.com>
2026-01-30 19:28:03 +0000

    addedtestingplaygroundfile

---
+++ emma_Main.py
@@ -0,0 +1,8 @@
+num = 12
+print("Welcome to the guessing game! guess a number between 1 and 20")
+guess = int(input("Enter your guess: "))
+if guess == num:
+    print("Congratulations! You guessed the correct number.")
+else:
+    print("Sorry, that's incorrect. The correct number was", num)
+    print("GOOGOOGAAAGAGAGGAGAGANOURSHITTYIHATEYOUSIGMASSOSOSSOSO")
