0f7f4b7b5e04b6ce1d952950430e53687ba336fa
Virginia <virginiaegreene@gmail.com>
2026-03-27 13:35:57 -0400

    Added rectangle button

--- virginia_main.py
+++ virginia_main.py
@@ -1,4 +1,19 @@
-print (I am the best )
+
+name = input("Enter your name: ")
+print(name)
+print ("i like")
+a = ("Dukesaw")
+b = ("Catradora")
+c=("lumity")
+
+print(a)
+print(b)
+print(c)
+guess =input("you like them?")
+if guess =="yes":
+    print("Congratulations! You guessed the correct number.")
+else:
+    print("aw man")
 if guess == num:
     print("Congratulations! You guessed the correct number.")
 else:
